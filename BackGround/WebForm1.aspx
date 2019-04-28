24c464c4dd05841c64e02156917ec2073cf6ba1e
owloliviakelly <50046877+owloliviakelly@users.noreply.github.com>
2019-04-28 22:11:37 +0700

    Добавьте файлы проекта.

---
+++ BackGround/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="BackGround.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body id="Body" runat="server"> 
+<form id="form1" runat="server"> 
+
+    <div>
+        
+       <button id="button1" runat="server" onserverclick="doIt" >Red</button>
+         <button id="button2" runat="server" onserverclick="doIt2" >yellow</button>
+             <button id="button3" runat="server" onserverclick="doIt3" >Green</button>
+                 <button id="button4" runat="server" onserverclick="doIt4" >Blue</button>
+        </div>
+</form> 
+</body>
+</html>
---
+++ BackGround/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NLog;
+
+
+namespace BackGround
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void doIt(object sender, EventArgs e)
+        {
+            Body.Attributes["bgcolor"] = "Red";
+            logger.Trace("Нажата кнопка красный");
+        }
+        protected void doIt2(object sender, EventArgs e)
+        {
+            Body.Attributes["bgcolor"] = "yellow";
+            logger.Trace("Нажата кнопка Желтый");
+        }
+        protected void doIt3(object sender, EventArgs e)
+        {
+            Body.Attributes["bgcolor"] = "green";
+            logger.Trace("Нажата кнопка зеленый");
+        }
+        protected void doIt4(object sender, EventArgs e)
+        {
+            Body.Attributes["bgcolor"] = "blue";
+            logger.Trace("Нажата кнопка синий");
+        }
+      
+    }
+}
